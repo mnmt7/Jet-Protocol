36dc15c5bd14f218685fe2ea38321a6289f83709
Manmeet <mnmtsnghh@gmail.com>
2023-10-09 16:04:16 +0530

    add font, colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        playfair: ["var(--font-playfair-display)"],
+      },
+
+      colors: {
+        "sage-green": "#64AE9D",
+        "sage-green-dark": "#49766c",
+      },
+
+      boxShadow: {
+        "inner-black-white":
+          "0px 2px 2px 0px #FFFFFF7A inset, 0px -2px 2px 0px #0000007A inset",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
