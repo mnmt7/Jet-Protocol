9a7dd78340563cd5aa4f98f0b8bf25e636530711
Manmeet <mnmtsnghh@gmail.com>
2023-10-09 22:03:40 +0530

    add cta page

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Cta = () => {
   return (
-    <>
-      <div>
+    <main className="pt-32">
+      <section>
         <Image
           src="/gradient.png"
           alt="gradient"
@@ -13,21 +13,21 @@ const Cta = () => {
           height={532}
           className="absolute top-14 sm:top- md:top-0 left-[50%] transform -translate-x-1/2 -z-10 w-[1000px] h-auto"
         />
-      </div>
+      </section>
 
-      <div className="pt-32 sm:pt-40 md:pt-44 lg:pt-52 text-center">
-        <div className="text-xs text-sage-green">JET PROTOCOL</div>
-        <div className="text-5xl mt-3">
+      <section className="sm:pt-40 md:pt-44 lg:pt-52 text-center">
+        <h1 className="text-xs text-sage-green">JET PROTOCOL</h1>
+        <h2 className="text-5xl mt-3">
           <span className="font-[500] ">the next generation of</span>
           <span className={`block font-playfair italic font-[600] mt-3`}>
             defi governance
           </span>
-        </div>
-        <div className="text-[#FFFFFF99] mt-6 text-xl md:text-2xl">
+        </h2>
+        <h2 className="text-[#FFFFFF99] mt-6 text-xl md:text-2xl">
           experience open source, transparent and efficient borrowing
           <br />
           and lending on solana.
-        </div>
+        </h2>
         <div className="flex gap-8 justify-center mt-12">
           <div className=" bg-sage-green hover:bg-sage-green-dark hover:cursor-pointer text-lg rounded-full text-black py-2 px-6 font-semibold shadow-inner-black-white">
             <Link href="/docs">read docs</Link>
@@ -36,8 +36,8 @@ const Cta = () => {
             <Link href="/paper">how it works</Link>
           </div>
         </div>
-      </div>
-    </>
+      </section>
+    </main>
   );
 };
 
