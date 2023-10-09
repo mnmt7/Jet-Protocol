f477314aa48c7e0f8179564f2ddc0c6f26917036
Manmeet <mnmtsnghh@gmail.com>
2023-10-09 21:59:49 +0530

    modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,22 @@
 import Nav from "@/components/Nav";
 import Cta from "@/components/Cta";
 import ImageScroller from "@/components/ImageScroller";
+import type { Metadata } from "next";
+import imageList from "@/utils/imageList";
+import Info from "@/components/Info";
+
+export const metadata: Metadata = {
+  title: "Home page",
+  description: "Home page for the Jet Protocol website.",
+};
 
 const Page = () => {
   return (
     <>
       <Nav />
       <Cta />
-      <ImageScroller
-        images={[
-          "image 18.png",
-          "image 19.png",
-          "image 20.png",
-          "image 21.png",
-          "image 22.png",
-          "image 23.png",
-          "image 24.png",
-          "image 25.png",
-          "image 26.png",
-          "image 18.png",
-          "image 19.png",
-          "image 20.png",
-          "image 21.png",
-          "image 22.png",
-          "image 23.png",
-          "image 24.png",
-          "image 25.png",
-          "image 26.png",
-        ]}
-      />
+      <ImageScroller images={imageList} />
+      <Info />
     </>
   );
 };
